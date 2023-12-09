abefd72e3c8edef57ea8e1ffc250d362c4639d62
Yasasree-Singam <114019539+Yasasree-Singam@users.noreply.github.com>
2023-12-09 17:49:12 -0500

    Update App.py

--- App.py
+++ App.py
@@ -386,7 +386,6 @@ page_names_to_funcs = {
     "Regression": page3,
     "Association_Rules": page4
                        }
-
-selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
 st.sidebar.header(" Regression, Classification, Association rule generation")
+selected_page = st.sidebar.selectbox("Select a page to toggle", page_names_to_funcs.keys())
 page_names_to_funcs[selected_page]()
