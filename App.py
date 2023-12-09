bc8cfe5dacfdbf05ee38c88f1d6737ae389e08b7
Yasasree-Singam <114019539+Yasasree-Singam@users.noreply.github.com>
2023-12-09 17:09:20 -0500

    Update App.py

--- App.py
+++ App.py
@@ -6,7 +6,7 @@ import geopandas as gpd
 import folium
 from folium.plugins import MarkerCluster
 from streamlit_folium import folium_static
-from classification import train_random_forest, train_svm, train_knn, plot_confusion_matrix
+from Classification.classification import train_random_forest, train_svm, train_knn, plot_confusion_matrix
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, ConfusionMatrixDisplay
 from Apriori_algorithm.apriori import runApriori, dataFromFile, to_str_results
 from Data_preprocessing.preprocessing import preprocess_data
