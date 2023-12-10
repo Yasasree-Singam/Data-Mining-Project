1b2b4b26beb3a73fe522efe395b6ef70fb2816ed
Yasasree-Singam <114019539+Yasasree-Singam@users.noreply.github.com>
2023-12-09 20:36:17 -0500

    Update App.py

--- App.py
+++ App.py
@@ -176,7 +176,7 @@ def collect_user_input(data_balance,X_train):
 
 
 def page1():
-    st.title("Los Angeles Crime")
+    st.title("Los Angeles Crime Prediction")
     # st.write(" Regression, Classification, Association rule generation")
     # Load your data
     data = pd.read_csv("Classification/cleaned_data.csv").drop(columns=["Unnamed: 0"], errors='ignore')
