796955cae2f592c84bc7f46c985e7382421f18c0
Konda Sai Divya Teja <69079789+saidivyateja@users.noreply.github.com>
2023-12-09 19:43:30 -0500

    Update App.py

--- App.py
+++ App.py
@@ -397,37 +397,46 @@ def page3():
 def page4():
     st.title("Association rules")
     # csv_file = pd.read_csv('combinedapriori.csv')
-    st.markdown('''
+    default_csv = st.selectbox(
+    "Select one of the sample csv files", ("combinedapriori.csv")
+)
+
+if default_csv == 'combinedapriori.csv':
+    # st.markdown('''The dataset is a copy of the “Online directory of certified businesses with a detailed profile” file from the 
+    # Small Business Services (SBS) dataset in the NYC Open Data Sets http://nycopendata.socrata.com/''')
+# elif default_csv == 'tesco.csv':
+#     st.markdown('The dataset is a toy dataset contain frequently purchased grocery items')
+
+# st.markdown('Here are some sample rows from the dataset')
+# csv_file = pd.read_csv(default_csv, header=None, lineterminator="\n")
+# st.write(csv_file[0].str.split("\,", expand=True).head())
+
+# st.markdown('---')
+# st.markdown("## Inputs")
+
+st.markdown('''
             **Support** shows transactions with items purchased together in a single transaction.
             
             **Confidence** shows transactions where the items are purchased one after the other.''')
 
-    st.markdown('Support and Confidence for Itemsets A and B can be represented by formulas')
+st.markdown('Support and Confidence for Itemsets A and B can be represented by formulas')
 
-    support_helper = ''' > Support(A) = (Number of transactions in which A appears)/(Total Number of Transactions') '''
-    confidence_helper = ''' > Confidence(A->B) = Support(AUB)/Support(A)') '''
-    st.markdown('---')
+support_helper = ''' > Support(A) = (Number of transactions in which A appears)/(Total Number of Transactions') '''
+confidence_helper = ''' > Confidence(A->B) = Support(AUB)/Support(A)') '''
+st.markdown('---')
 
-    support = st.slider("Enter the Minimum Support Value", min_value=0.1,
+support = st.slider("Enter the Minimum Support Value", min_value=0.1,
                     max_value=0.9, value=0.15,
                     help=support_helper)
 
-    confidence = st.slider("Enter the Minimum Confidence Value", min_value=0.1,
+confidence = st.slider("Enter the Minimum Confidence Value", min_value=0.1,
                        max_value=0.9, value=0.6, help=confidence_helper)
 
-    # Example usage in a Streamlit app
-    # inFile = st.file_uploader('Upload File', type=['csv'])
-    
-    # if inFile is not None:
-    #     for record in dataFromFile(inFile):
-    #         # Process each record as needed
-    #         st.write(record)
+inFile = dataFromFile(default_csv)
 
-    inFile = dataFromFile('combinedapriori.csv')
+items, rules = runApriori(inFile, support, confidence)
 
-    items, rules = runApriori(inFile, support, confidence)
-
-    i, r, original_rules = to_str_results(items, rules)
+i, r = to_str_results(items, rules)
 
     st.markdown("## Results")
 
