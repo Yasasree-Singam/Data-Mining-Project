3a3a6d046bbcbb5ca59672bf4c18b3182bc268db
Konda Sai Divya Teja <69079789+saidivyateja@users.noreply.github.com>
2023-12-09 19:47:24 -0500

    Update App.py

--- App.py
+++ App.py
@@ -413,27 +413,27 @@ def page4():
 
 # st.markdown('---')
 # st.markdown("## Inputs")
-st.markdown('''**Support** shows transactions with items purchased together in a single transaction.
-**Confidence** shows transactions where the items are purchased one after the other.''')
-
-st.markdown('Support and Confidence for Itemsets A and B can be represented by formulas')
-
-support_helper = ''' > Support(A) = (Number of transactions in which A appears)/(Total Number of Transactions') '''
-confidence_helper = ''' > Confidence(A->B) = Support(AUB)/Support(A)') '''
-st.markdown('---')
-
-support = st.slider("Enter the Minimum Support Value", min_value=0.1,
-                    max_value=0.9, value=0.15,
-                    help=support_helper)
-
-confidence = st.slider("Enter the Minimum Confidence Value", min_value=0.1,
-                       max_value=0.9, value=0.6, help=confidence_helper)
-
-inFile = dataFromFile(default_csv)
-
-items, rules = runApriori(inFile, support, confidence)
-
-i, r = to_str_results(items, rules)
+    st.markdown('''**Support** shows transactions with items purchased together in a single transaction.
+    **Confidence** shows transactions where the items are purchased one after the other.''')
+    
+    st.markdown('Support and Confidence for Itemsets A and B can be represented by formulas')
+    
+    support_helper = ''' > Support(A) = (Number of transactions in which A appears)/(Total Number of Transactions') '''
+    confidence_helper = ''' > Confidence(A->B) = Support(AUB)/Support(A)') '''
+    st.markdown('---')
+    
+    support = st.slider("Enter the Minimum Support Value", min_value=0.1,
+                        max_value=0.9, value=0.15,
+                        help=support_helper)
+    
+    confidence = st.slider("Enter the Minimum Confidence Value", min_value=0.1,
+                           max_value=0.9, value=0.6, help=confidence_helper)
+    
+    inFile = dataFromFile(default_csv)
+    
+    items, rules = runApriori(inFile, support, confidence)
+    
+    i, r = to_str_results(items, rules)
 
     st.markdown("## Results")
 
